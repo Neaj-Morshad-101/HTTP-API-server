c3058b5f072dee4c3b1c7786033368e48031a5b7
neaj <neaj@appscode.com>
2023-03-23 20:51:27 +0600

    Latest Working and Tested with Authentication

--- go.mod
+++ go.mod
@@ -3,6 +3,21 @@ module github.com/Neaj-Morshad-101/HTTP-API-server
 go 1.20
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/jwtauth v1.2.0
+	github.com/gorilla/mux v1.8.0
+	github.com/lestrrat-go/jwx v1.2.25
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.1 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 )
--- main.go
+++ main.go
@@ -23,20 +23,29 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/lestrrat-go/jwx/jwa"
 	"log"
 	"net/http"
 	"sort"
 	"strconv"
-	//"fmt"
-	//"time"
-	//
-	//"github.com/go-chi/chi/v5"
-	//"github.com/go-chi/chi/v5/middleware"
-	//"github.com/go-chi/jwtauth/v5"
-	//"github.com/lestrrat-go/jwx/jwa"
-	//"github.com/lestrrat-go/jwx/jwt"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 )
+
+var jwtkey = []byte("Neaj's Secret Key, He will not share it")
+var tokenAuth *jwtauth.JWTAuth
+var tokenString string
+var token jwt.Token
+
+type credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 // Album Struct
 type Album struct {
@@ -57,6 +66,17 @@ var maxAlbumID int
 
 // Init a albums var as a slice of Album struct
 var albums []Album
+var credslist map[string]string
+
+func initCreds() {
+	tokenAuth = jwtauth.New(string(jwa.HS256), jwtkey, nil)
+	credslist = make(map[string]string)
+	creds := credentials{
+		"Neaj Morshad",
+		"1234",
+	}
+	credslist[creds.Username] = creds.Password
+}
 
 func initMaxID() {
 	maxAlbumID = 0
@@ -111,13 +131,15 @@ func initAlbumDB() {
 //"language": "Bangla"
 //}
 
-func initDB() {
+func initAll() {
+	initCreds()
 	initMaxID()
 	initAlbumDB()
 }
 
 // Get All Albums
 func getAlbums(w http.ResponseWriter, r *http.Request) {
+	log.Println("Called: getAlbums()")
 	w.Header().Set("Content-Type", "application/json")
 	sort.SliceStable(albums, func(i, j int) bool {
 		return albums[i].ID < albums[j].ID
@@ -128,9 +150,13 @@ func getAlbums(w http.ResponseWriter, r *http.Request) {
 // Get Single Album
 func getAlbum(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get the params
+	param := chi.URLParam(r, "id")
+	paramsID, _ := strconv.Atoi(param)
+	//
+	//params := mux.Vars(r) // Get the params
+	//
+	//paramsID, _ := strconv.Atoi(params["id"])
 
-	paramsID, _ := strconv.Atoi(params["id"])
 	//Loop through albums and find that id
 	for _, curAlbum := range albums {
 		if curAlbum.ID == paramsID {
@@ -161,8 +187,10 @@ func createAlbum(w http.ResponseWriter, r *http.Request) {
 
 func updateAlbum(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get the params
-	paramsID, _ := strconv.Atoi(params["id"])
+	param := chi.URLParam(r, "id")
+	paramsID, _ := strconv.Atoi(param)
+	//params := mux.Vars(r) // Get the params
+	//paramsID, _ := strconv.Atoi(params["id"])
 
 	var newAlbum Album
 	err := json.NewDecoder(r.Body).Decode(&newAlbum)
@@ -179,28 +207,36 @@ func updateAlbum(w http.ResponseWriter, r *http.Request) {
 			newAlbum.ID = paramsID
 
 			albums = append(albums, newAlbum)
+			sort.SliceStable(albums, func(i, j int) bool {
+				return albums[i].ID < albums[j].ID
+			})
+			json.NewEncoder(w).Encode(albums)
 
-			break
+			return
 		}
 	}
-	sort.SliceStable(albums, func(i, j int) bool {
-		return albums[i].ID < albums[j].ID
-	})
-	json.NewEncoder(w).Encode(albums)
+
+	json.NewEncoder(w).Encode("No album data available for the given id")
+
 }
 func deleteAlbum(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get the params
-	paramsID, _ := strconv.Atoi(params["id"])
+	//params := mux.Vars(r) // Get the params
+
+	param := chi.URLParam(r, "id")
+	paramsID, _ := strconv.Atoi(param)
+	fmt.Println("------------------>paramid", paramsID)
 
 	//Loop through albums and find that id
 	for index, curAlbum := range albums {
 		if curAlbum.ID == paramsID {
+			fmt.Println("----------------->hi")
 			//time complexity can be improved by using map
 			albums = append(albums[:index], albums[index+1:]...)
 			break
 		}
 	}
+	fmt.Println(albums)
 
 	json.NewEncoder(w).Encode(albums)
 }
@@ -225,9 +261,11 @@ func getArtists(w http.ResponseWriter, r *http.Request) {
 // Get Top X Artist by album total number of album count
 func getTopArtists(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get the params
+	param := chi.URLParam(r, "cnt")
+	//paramsID, _ := strconv.Atoi(param)
+	//params := mux.Vars(r) // Get the params
 
-	numberOfArtistToShow, _ := strconv.Atoi(params["x"])
+	numberOfArtistToShow, _ := strconv.Atoi(param)
 
 	var artistCount = make(map[Artist]int)
 
@@ -261,23 +299,100 @@ func getTopArtists(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(artistList)
 }
 
+func Login(w http.ResponseWriter, r *http.Request) {
+
+	var creds credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+
+	fmt.Println(creds)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	correctPassword, ok := credslist[creds.Username]
+
+	if !ok || creds.Password != correctPassword {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	//tokenAuth = jwtauth.New(string(jwa.HS256), jwtkey, nil)
+
+	expiretime := time.Now().Add(2 * time.Minute)
+
+	_, tokenString, err := tokenAuth.Encode(map[string]interface{}{
+		"aud": "Neaj Morshad",
+		"exp": expiretime.Unix(),
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "jwt",
+		Value:   tokenString,
+		Expires: expiretime,
+	})
+	w.WriteHeader(http.StatusOK)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:    "jwt",
+		Expires: time.Now(),
+	})
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	//Init Router
-	router := mux.NewRouter()
+	fmt.Println("started")
 
 	//Giving Initial Data
-	initDB()
+	initAll()
 
-	//Route Handlers /Endpoints
-	router.HandleFunc("/api/albums", getAlbums).Methods("GET")
-	router.HandleFunc("/api/albums/{id}", getAlbum).Methods("GET")
-	router.HandleFunc("/api/albums", createAlbum).Methods("POST")
-	router.HandleFunc("/api/albums/{id}", updateAlbum).Methods("PUT")
-	router.HandleFunc("/api/albums/{id}", deleteAlbum).Methods("DELETE")
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Post("/login", Login)
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(jwtauth.Authenticator)
 
-	router.HandleFunc("/api/artists", getArtists).Methods("GET")
-	router.HandleFunc("/api/artists/{x}", getTopArtists).Methods("GET")
+		//	r.Use(middleware.BasicAuth("user", credslist))
 
-	log.Fatal(http.ListenAndServe(":5050", router))
+		r.Route("/albums", func(r chi.Router) {
+			r.Get("/", getAlbums)
+			r.Get("/{id}", getAlbum)
+			r.Post("/", createAlbum)
+			r.Delete("/{id}", deleteAlbum)
+			r.Put("/{id}", updateAlbum)
+		})
+
+		r.Route("/artists", func(r chi.Router) {
+			r.Get("/", getArtists)
+			r.Get("/{cnt}", getTopArtists)
+		})
+		r.Post("/logout", Logout)
+
+	})
+	http.ListenAndServe(":5050", r)
+
+	//previous code using max
+	//router := max.NewRouter()
+	////Route Handlers /Endpoints
+	//router.HandleFunc("/albums", getAlbums).Methods("GET")
+	//router.HandleFunc("/albums/{id}", getAlbum).Methods("GET")
+	//router.HandleFunc("/albums", createAlbum).Methods("POST")
+	//router.HandleFunc("/albums/{id}", updateAlbum).Methods("PUT")
+	//router.HandleFunc("/albums/{id}", deleteAlbum).Methods("DELETE")
+	//
+	//router.HandleFunc("/artists", getArtists).Methods("GET")
+	//router.HandleFunc("/artists/{x}", getTopArtists).Methods("GET")
 
 }
